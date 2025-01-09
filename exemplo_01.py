94896bc10280323780fb16874c5328c30bc26543
Caio Souza <caiohas.2908@gmail.com>
2025-01-09 17:05:35 -0300

    commit do fim da aula

--- exemplo_01.py
+++ exemplo_01.py
@@ -1,1 +1,3 @@
-print("Hello World! #2")
+# Crie um programa que o usuário digita dois valores e apareça a soma
+
+print(int(input("Digite o primeiro numero: ")) + int(input("Digite o segundo numero: ")))
---
+++ exemplo_02.py
@@ -0,0 +1,5 @@
+# Refatore o exercício 00 utilizando variáveis
+
+nome = input("Digite o seu nome: ")
+quantidade_caracteres = len(nome)
+print(quantidade_caracteres)
